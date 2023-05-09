7c03e45134422e5af3851d8d794c6ed44122c94d
Daibert Ramirez <126725296+Daibertj@users.noreply.github.com>
2023-05-09 22:15:44 +0000

    Simple Counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [counter, setCounter] = useState(0)
+
+	useEffect(() => {
+		let setIntervalId = setInterval(() => {
+			setCounter(counter + 1)
+		}, 1000)
+
+		return () => {
+			clearInterval(setIntervalId)
+		}
+
+	}, [counter])
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container-fluid bigCounter  mt-3 bg-dark text-white">
+				
+				<div className="d-flex">
+					<h1 className="d-flex p-4 w-25 m-5 bg-gradient rounded">
+						<i class="far fa-clock"></i>
+					</h1>
+					<h1 className="w-25 p-4 m-5 bg-gradient rounded">{Math.floor(counter / 100000) % 10}</h1>
+					<h1 className="w-25 p-4 m-5 bg-gradient rounded">{Math.floor(counter / 10000) % 10}</h1>
+					<h1 className="w-25 p-4 m-5 bg-gradient rounded">{Math.floor(counter / 1000) % 10}</h1>
+					<h1 className="w-25 p-4 m-5 bg-gradient rounded">{Math.floor(counter / 100) % 10}</h1>
+					<h1 className="w-25 p-4 m-5 bg-gradient rounded">{Math.floor(counter / 10) % 10}</h1>
+					<h1 className="w-25 p-4 m-5 bg-gradient rounded">{Math.floor(counter / 1) % 10}</h1>
+				</div>
+			</div>
+		</>
 	);
 };
 
